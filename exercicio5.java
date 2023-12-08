12a0dd161b801a917cbddfbc5bef1a32ae417ea2
cintia silva <cintiasilva7300@gmail.com>
2023-12-08 14:41:12 -0300

    update

--- exercicio5.java
+++ exercicio5.java
@@ -12,19 +12,39 @@ public class exercicio5 {
 	        
 	        System.out.println("Digite um valor para somar, e digite '0' para executar a soma e finalizar o programa. " );
 
+//	        while (true) {
+//	        	numero = scanner.nextInt();
+//	        	
+//	        	total += numero;
+//
+//	            if (numero == 0) {  	
+//		        	System.out.println("total: " + total);
+//	          
+//	            	System.out.println("Programa encerrado!");
+//	                break; 
+//	            }else if(numero < 0){
+//	            	System.out.println("Valor menor que zero");
+//	            }
+//	        }
+	        
 	        while (true) {
-	        	numero = scanner.nextInt();
-	        	
-	        	total += numero;
+	            numero = scanner.nextInt();
 
-	            if (numero == 0) {  	
-		        	System.out.println("total: " + total);
-	          
-	            	System.out.println("Programa encerrado!");
-	                break; 
+	            if (numero < 0) {
+	                System.out.println("Número negativo detectado. Não será somado.");
+	            } else {
+	                total += numero;
 	            }
-	        }
 
+	            if (numero == 0) {
+	                System.out.println("Total: " + total);
+	                System.out.println("Programa encerrado!");
+	                break;
+	            } else {
+	                System.out.println("Digite outro valor (ou '0' para encerrar):");
+	            }
+	      
+	    }
 	}
 
 }
